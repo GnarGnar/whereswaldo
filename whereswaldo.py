f63d9f251c4ccbab9fd782406ccd034484575e72
GnarGnar <kburke@csumb.edu>
2016-12-03 11:13:32 -0800

    waldos

---
+++ whereswaldo.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+waldo1 = Image.open("swamp_waldo.jpg")
+waldo2 = Image.open("waldo_dinosaurs.jpg")
+waldo3 = Image.open("waldo_store.jpg")
+waldo4 = Image.open("waldo_streets.jpg")
+waldo5 = Image.open("waldo_wizards.jpg")
+waldo6 = Image.open("maps_troy_waldo")
+
+waldo1.load()
+waldo2.load()
+waldo3.load()
+waldo4.load()
+waldo5.load()
+waldo6.load()
+
+
+
+waldo6.show()
