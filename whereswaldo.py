9dc84da8e75b686a9319c312a4aed204d87f7d45
GnarGnar <kburke@csumb.edu>
2016-12-03 12:44:59 -0800

    Lines on waldo1

--- whereswaldo.py
+++ whereswaldo.py
@@ -1,12 +1,12 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 
 waldo1 = Image.open("swamp_waldo.jpg")
 waldo2 = Image.open("waldo_dinosaurs.jpg")
 waldo3 = Image.open("waldo_store.jpg")
 waldo4 = Image.open("waldo_streets.jpg")
 waldo5 = Image.open("waldo_wizards.jpg")
-waldo6 = Image.open("maps_troy_waldo")
+waldo6 = Image.open("maps_troy.jpg")
 
 waldo1.load()
 waldo2.load()
@@ -15,6 +15,32 @@ waldo4.load()
 waldo5.load()
 waldo6.load()
 
+def waldo1func():
 
+	draw = ImageDraw.Draw(waldo1)
+	draw.line((450,490) + (481,490), fill = (0,255,0), width = 5)
+	draw.line((450,515) + (481,515), fill = (0,255,0), width =5)
+	draw.line((450,490) + (450,515), fill = (0,255,0),width = 5)
+	draw.line((481,490) + (481,515), fill =(0,255,0),width =5)
+	del draw
+	waldo1.show()
+	width, height = waldo1.size
+	print (width)
+	print (height)
+	
+	
+	
+def waldo2func():
+	waldo2.show()
+	
+def waldo3func():
+	waldo3.show()
+	
+def waldo4func():
+	waldo4.show()
+	
+def waldo5func():
+	waldo5.show()
+	
+waldo1func()
 
-waldo6.show()
